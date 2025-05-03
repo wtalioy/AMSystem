8fcb14a024cb0e2778d5a7f0ca6834c66398cee3
WangRuiming <23302010017@m.fudan.edu.cn>
2025-05-03 14:44:53 +0800

    refactor: remove unnecessary env proprocess

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,6 +1,5 @@
 import os
-from pydantic import BaseSettings, AnyHttpUrl, validator
-from typing import Optional, List
+from pydantic import BaseSettings
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -10,22 +9,6 @@ class Settings(BaseSettings):
     SECRET_KEY: str = os.getenv("SECRET_KEY", "")
     ALGORITHM: str = os.getenv("ALGORITHM", "HS256")
     ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "30"))
-    
-    # MYSQL配置
-    MYSQL_USER: str = os.getenv("MYSQL_USER", "root")
-    MYSQL_PASSWORD: str = os.getenv("MYSQL_PASSWORD", "password")
-    MYSQL_SERVER: str = os.getenv("MYSQL_SERVER", "localhost")
-    MYSQL_PORT: str = os.getenv("MYSQL_PORT", "3306")
-    MYSQL_DB: str = os.getenv("MYSQL_DB", "ams_db")
-    
-    # 数据库URL
-    SQLALCHEMY_DATABASE_URI: Optional[str] = None
-
-    @validator("SQLALCHEMY_DATABASE_URI", pre=True)
-    def assemble_db_connection(cls, v: Optional[str], values: dict) -> str:
-        if isinstance(v, str):
-            return v
-        return f"mysql+pymysql://{values.get('MYSQL_USER')}:{values.get('MYSQL_PASSWORD')}@{values.get('MYSQL_SERVER')}:{values.get('MYSQL_PORT')}/{values.get('MYSQL_DB')}"
     
     class Config:
         case_sensitive = True
