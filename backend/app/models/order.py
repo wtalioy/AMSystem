5e1c55f1dfc193e8fba331b7ff465ce32d273538
WangRuiming <23302010017@m.fudan.edu.cn>
2025-05-03 14:36:47 +0800

    style: modify status annotation

--- backend/app/models/order.py
+++ backend/app/models/order.py
@@ -9,8 +9,8 @@ class Order(Table):
     description = Column(Text, nullable=False)
     rating = Column(Integer, check='BETWEEN 1 AND 5', nullable=True)
     comment = Column(Text, nullable=True)
-    status = Column(Integer, nullable=False, default=0)  # 0: pending, 1: ongoing, 2: completed
-    
+    status = Column(Integer, nullable=False, default=0)  # 0: pending, 1: in progress, 2: completed
+
     worker_id = Column(Char(10), foreign_key='Worker.user_id', nullable=False)
     car_id = Column(Char(10), foreign_key='Car.car_id', nullable=False)
     customer_id = Column(Char(10), foreign_key='Customer.user_id', nullable=False)
--- backend/app/models/procedure.py
+++ backend/app/models/procedure.py
@@ -5,6 +5,6 @@ class Procedure(Table):
 
     procedure_id = Column(Integer, primary_key=True, autoincrement=True)
     procedure_text = Column(TinyText, nullable=False)
-    current_status = Column(Integer, nullable=False, default=1)  # 1: ongoing, 2: finished
-    
+    current_status = Column(Integer, nullable=False, default=0)  # 0: pending, 1: in progress, 2: completed
+
     order_id = Column(Char(10), primary_key=True, foreign_key='Order.order_id')
